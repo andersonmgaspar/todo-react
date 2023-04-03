da4af61cd203fc578040be2e22ca5fc8c5a40feb
Andi <andersonmgaspar@gmail.com>
2023-04-03 15:33:32 -0300

    housekeeping counterbutton

--- src/components/counter/CounterButton.jsx
+++ src/components/counter/CounterButton.jsx
@@ -1,32 +1,16 @@
-import { useState } from 'react'
 import { PropTypes } from 'prop-types'
 
 export default function CounterButton({by, incrementTotal, decrementTotal}) {
     
-    const [count, setCount] = useState(0);
-
-    function incrementCounter() {
-        setCount(count + by)
-        incrementTotal(by)
-        console.log('increment clicked')
-    }
-
-    function decreaseCounter() {
-        setCount(count - by)
-        decrementTotal(by)
-        console.log('decrease clicked')
-    }
-    
     return (
         <div className="Counter">
-            
             <div>
                 <button className="counterButton"
-                        onClick={incrementCounter}>
+                        onClick={() => incrementTotal(by)}>
                     +{by}
                 </button>
                 <button className="counterButton"
-                        onClick={decreaseCounter}>
+                        onClick={() => decrementTotal(by)}>
                     -{by}
                 </button>
             </div>
